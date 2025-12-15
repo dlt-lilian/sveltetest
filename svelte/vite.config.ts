960b3bbca2ddd8ae83f1b5dbefdb35f46b262920
dlt-lilian <dulotlilian@gmai.com>
2025-12-15 20:34:23 +0100

    mofig

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -1,6 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig, preview } from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
