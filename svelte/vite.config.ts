92b5ed48004015ab304af60b323851e8ec61f6b6
dlt-lilian <dulotlilian@gmai.com>
2025-12-15 20:40:40 +0100

    MAJ

--- svelte/vite.config.ts
+++ svelte/vite.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
 	preview: {
-		allowedHosts: ['https://sveltetest-production.up.railway.app'],
+		allowedHosts: ['sveltetest-production.up.railway.app'],
 	}
 });
